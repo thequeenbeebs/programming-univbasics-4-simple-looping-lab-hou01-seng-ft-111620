20bc2041c0cebca8c5f444d8263248c4315eee86
Blaire Baker <bakerblaire@gmail.com>
2020-10-21 19:10:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,8 +25,9 @@ end
 
 def return_string_array (array)
   counter = 0
+  new_array = []
   while counter < array.length do
-    array[counter].to_s
+    new_array[counter] = array[counter].to_s
     counter += 1
   end
 end
